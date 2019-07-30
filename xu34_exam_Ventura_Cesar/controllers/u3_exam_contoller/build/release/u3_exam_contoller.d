094f0c477f2aacc93c8e8d4773a8d1fd4b52d929
1701027 <al1701027@upy.edu.mx>
2019-07-29 22:31:44 -0500

    finished

---
+++ xu34_exam_Ventura_Cesar/controllers/u3_exam_contoller/build/release/u3_exam_contoller.d
@@ -0,0 +1,7 @@
+build/release/u3_exam_contoller.o: u3_exam_contoller.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/keyboard.h
--- xu34_exam_Ventura_Cesar/controllers/u3_exam_contoller/u3_exam_contoller.c
+++ xu34_exam_Ventura_Cesar/controllers/u3_exam_contoller/u3_exam_contoller.c
@@ -1,63 +1,70 @@
-/*
- * File:          u3_exam_contoller.c
- * Date:
- * Description:
- * Author:
- * Modifications:
- */
-
-/*
- * You may need to add include files like <webots/distance_sensor.h> or
- * <webots/differential_wheels.h>, etc.
- */
 #include <webots/robot.h>
+#include <webots/motor.h>
+#include <webots/distance_sensor.h>
+#include <webots/keyboard.h>
 
+#include <stdio.h>
 /*
  * You may want to add macros here.
  */
 #define TIME_STEP 64
+#define VELOCITY 3
+#define VEL_INCREMENT 0.2
 
-/*
- * This is the main program.
- * The arguments of the main function can be specified by the
- * "controllerArgs" field of the Robot node
- */
-int main(int argc, char **argv)
-{
-  /* necessary to initialize webots stuff */
+int pressed_key;
+double velocity;
+double VelInc;
+
+
+double Velocity (double V) {
+  
+double velocity; 
+
+    velocity = V/0.075;
+    return(velocity);
+    }
+    
+double IncVel (double i) {
+
+double VelInc;
+
+    VelInc = i/0.075;
+    return(VelInc);
+    }
+
+int main(int argc, char **argv) {
+  
   wb_robot_init();
 
-  /*
-   * You should declare here WbDeviceTag variables for storing
-   * robot devices like this:
-   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
-   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
-   */
-
-  /* main loop
-   * Perform simulation steps of TIME_STEP milliseconds
-   * and leave the loop when the simulation is over
-   */
+ 
+   WbDeviceTag left = wb_robot_get_device ("motor_left");
+   WbDeviceTag right = wb_robot_get_device ("motor_right");
+   
+   WbDeviceTag position1 = wb_robot_get_device ("encoder1");
+   WbDeviceTag position2 = wb_robot_get_device ("encoder2");
+   
+   WbDeviceTag dis_left = wb_robot_get_device ("distance_senosor1");
+   WbDeviceTag dis_right = wb_robot_get_device ("distance_sensor2");
+  
+   wb_motor_set_position(left, INFINITY);
+   wb_motor_set_position(right, INFINITY);
+      
+   wb_distance_sensor_enable(dis_left, TIME_STEP);
+   wb_distance_sensor_enable(dis_right, TIME_STEP);
+   
+   wb_keyboard_enable(TIME_STEP);
+   
+    
   while (wb_robot_step(TIME_STEP) != -1) {
-
-    /*
-     * Read the sensors :
-     * Enter here functions to read sensor data, like:
-     *  double val = wb_distance_sensor_get_value(my_sensor);
-     */
-
-    /* Process sensor data here */
-
-    /*
-     * Enter here functions to send actuator commands, like:
-     * wb_differential_wheels_set_speed(100.0,100.0);
-     */
-  };
-
-  /* Enter your cleanup code here */
-
-  /* This is necessary to cleanup webots resources */
-  wb_robot_cleanup();
-
-  return 0;
-}
+  
+    velocity = Velocity(VELOCITY);
+    VelInc = IncVel(VEL_INCREMENT);
+    
+    pressed_key = wb_keyboard_get_key();
+    
+    if (pressed_key == 'S') {
+       velocity = velocity + VelInc;
+    }
+     else if (pressed_key == 'A') {
+      velocity = velocity - VelInc;
+     }
